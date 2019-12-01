c70f26f186d1f5fae29aa0a309ba68eb0f90485b
Krzysztof Wroblewski <behemius@gmail.com>
2019-12-01 18:05:40 +0100

    first test

--- ec2_http/main.tf
+++ ec2_http/main.tf
@@ -4,6 +4,7 @@ provider "aws" {
 
 data "aws_ami" "latest-amazon-linux2" {
     most_recent = true
+    owners = ["137112412989"] # Amazon
     
     filter {
         name = "name"
@@ -14,4 +15,24 @@ data "aws_ami" "latest-amazon-linux2" {
     name   = "virtualization-type"
     values = ["hvm"]
   }
+    
+    filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+}
+
+resource "aws_instance" "http_server" {
+  #ami = "ami-0c6b1d09930fac512"
+  ami = "${data.aws_ami.latest-amazon-linux2.id}"
+  #instance_type = "t2.micro"
+  instance_type = "${var.instance_flavor}"
+
+  tags = {
+    Name = "http_server"
+  }
+}
+
+output "http_server_public_hostname" {
+  value = "${aws_instance.http_server.public_dns}"
 }
