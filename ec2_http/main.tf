d57a3afd7e13203fccae94d80f7694ae8c76c8ed
Krzysztof Wroblewski <behemius@gmail.com>
2019-12-01 19:00:12 +0100

    www server + security group

--- ec2_http/main.tf
+++ ec2_http/main.tf
@@ -32,6 +32,49 @@ resource "aws_instance" "http_server" {
   }
 }
 
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum update -y",
+      "sudo yum install apache2 -y",
+      "sudo systemctl enable apache2",
+      "sudo systemctl start apache2",
+      "sudo chmod 777 /var/www/html/index.html"
+    ]
+  }
+
+  provisioner "file" {
+    source = "index.html"
+    destination = "/var/www/html/index.html"
+  }
+    
+  provisioner "remote-exec" {
+    inline = [
+      "sudo chmod 644 /var/www/html/index.html"
+    ]
+  }
+
+resource "aws_security_group" "http_server" {
+  name = "http_server"
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 80
+    to_port     = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    protocol    = -1
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 output "http_server_public_hostname" {
   value = aws_instance.http_server.public_dns
+}
+
+output "http_server_public_ip" {
+  value = "${aws_instance.web_server.public_ip}"
 }
